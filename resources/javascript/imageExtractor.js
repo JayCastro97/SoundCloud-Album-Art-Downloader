481ad15abf054d361745bcdae1b522bb2a025e5b
Jay Castro <33559928+JayCastro97@users.noreply.github.com>
2019-07-19 19:54:00 -0500

    add download function

---
+++ resources/javascript/imageExtractor.js
@@ -0,0 +1,9 @@
+function download(){
+	var span = document.querySelector("span.sc-artwork.sc-artwork-placeholder-2.image__full");
+	var image = obj.style.backgroundImage;
+
+
+}
+
+
+//"background-image: url("https://i1.sndcdn.com/artworks-000569624996-ofkp2c-t500x500.jpg"); width: 100%; height: 100%;
