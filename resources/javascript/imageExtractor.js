632b68eddd3de4fed3db89d8b095f0996be951a3
Jay Castro <33559928+JayCastro97@users.noreply.github.com>
2019-07-21 23:56:43 -0500

    use XHR request

--- resources/javascript/imageExtractor.js
+++ resources/javascript/imageExtractor.js
@@ -1,3 +1,49 @@
+console.log("CONNECTED");
+var text = document.querySelector("input[type='text']");
+var button = document.querySelector("button");
+var url;
+
+button.addEventListener("click", function(){
+	console.log("url: " + text.value);
+	get(text.value).then(function(response) {
+	  console.log("Success!", response);
+	}, function(error) {
+	  console.error("Failed!", error);
+	});
+
+});
+
+
+
+
+function get(url) {
+// Return a new promise.
+return new Promise(function(resolve, reject) {
+	// Do the usual XHR stuff
+	var req = new XMLHttpRequest();
+	req.open('GET', url);
+
+    req.onload = function() {
+      // 200 = HTTP Success Status Code
+      if (req.status == 200) {
+        // Resolve the promise with the response text
+        resolve(req.response);
+      }
+      else {
+        // Otherwise reject with the status text
+        // which will hopefully be a meaningful error
+        reject(Error(req.statusText));
+      }
+    };
+    // Handle network errors
+    req.onerror = function() {
+      reject(Error("Network Error"));
+    };
+
+    // Make the request
+    req.send();
+  });
+}
 function download(){
 	var span = document.querySelector("span.sc-artwork.sc-artwork-placeholder-2.image__full");
 	var image = obj.style.backgroundImage;
